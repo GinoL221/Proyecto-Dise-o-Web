4555aaf04903b92a2c59978d4d327981d5d4a851
Gino <ginolencina@gmail.com>
2024-11-10 00:25:50 -0300

    Agregar ScrollReveal para animaciones en la landing page

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,15 @@ menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navList.classList.toggle("open");
 };
+
+const sr = ScrollReveal({
+  distance: "65px",
+  durantion: 2000,
+  delay: 450,
+  reset: true,
+});
+
+sr.reveal(".hero-text", { delay: 200, origin: "top" });
+sr.reveal(".hero-img", { delay: 450, origin: "top" });
+sr.reveal(".icons", { delay: 500, origin: "top" });
+sr.reveal(".scroll-down", { delay: 450, origin: "top" });
